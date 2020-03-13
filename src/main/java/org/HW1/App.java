a1527ac3c53135135ba83e9848ec62b3e395ff73
Shahar Stahi <shaharstahi@gmail.com>
2020-03-13 17:14:08 +0200

    Added comments

--- src/main/java/org/HW1/App.java
+++ src/main/java/org/HW1/App.java
@@ -15,19 +15,16 @@ public class App {
     }
 
     static Double calculate(String exp) {
-        //System.out.println("'" + exp + "'");
 
-        if (exp.equals(""))
-            return 0.0;
-        int ind;
-
-        ind = exp.indexOf(")");
+        // parenthesis case
+        int ind = exp.indexOf(")");
         if (ind != -1)
         {
+            // ind is the index of the firs ')' and the loop finds the matching '('
             int lp = ind - 1;
             while (exp.charAt(lp) != '(')
                 lp--;
-
+            // then we calculate the value within the parenthesis, adding it back to the expression and call recursively
             return calculate(exp.substring(0, lp) + calculate(exp.substring(lp + 1, ind)) + exp.substring(ind + 1));
         }
 
@@ -35,7 +32,9 @@ public class App {
         if (ind != -1)
             return calculate(exp.substring(0, ind).trim()) + calculate(exp.substring(ind + 1).trim());
 
-
+        // - case: we look for all - and prioritizing all cases:
+        // 1) case of num1 - num2, in this case we need to execute '-'
+        // 2) case of num1 */ - num2 (i.e 5 * -2), in this case we need to prioritize * or /
         for (int i = 1; i < exp.length(); i = Math.max(i + 1, ind))
         {
             ind = exp.indexOf("-", i);
@@ -49,6 +48,7 @@ public class App {
                     return calculate(exp.substring(0, ind)) - calculate(exp.substring(ind + 1));
             }
         }
+        // 3) case of - in the beginning of the expression. (here we know there are no more - in the expression since we took care of them in the prev if
         ind = exp.indexOf("-");
         if (ind != -1 && !exp.contains("*") && !exp.contains("/"))
             return -calculate(exp.substring(1));
